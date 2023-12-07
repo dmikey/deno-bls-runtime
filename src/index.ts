9c3530e6aa0e85198d8e4f0695d05fe300c1b1b5
Derek Anderson <dmikey@users.noreply.github.com>
2023-12-07 15:53:03 -0600

    just any for now

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,33 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 import { executeScript } from "./deno.ts";
 import { executeWasm } from "./wasm.ts"; // Import the function to execute WebAssembly files
 
-async function main() {
-    const args = parse(Deno.args);
+// Version of your CLI application
+const CLI_VERSION = "1.0.0";
 
+async function main() {
+    const args = parse(Deno.args, {
+        boolean: ["v", "version"],
+        alias: { v: "version" }
+    });
+
+    // Check for version flag
+    if (args.version) {
+        console.log(`CLI Version: ${CLI_VERSION}`);
+        return;
+    }
+
+    // Check if a file path is provided
     if (args._.length === 0) {
         console.error("No file path provided.");
         return;
     }
 
-    const filePath = args._[0];
+    const filePath:any = args._[0];
     try {
-        if ((filePath as any).endsWith(".ts")) {
-            await executeScript(filePath as any); // Call to `.ts` files
-        } else if ((filePath as any).endsWith(".wasm")) {
-            await executeWasm(filePath as any); // Call the function to execute WebAssembly files
+        if (filePath.endsWith(".ts")) {
+            await executeScript(filePath); // Call to `.ts` files
+        } else if (filePath.endsWith(".wasm")) {
+            await executeWasm(filePath); // Call the function to execute WebAssembly files
         } else {
             console.error("Unsupported file type.");
         }
